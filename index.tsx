ef82a5e19d8a2d5d08ec3e18a2441b328cb675cf
IdesignerSe <hebera75@gmail.com>
2023-10-20 11:53:11 +0200

    short update
    some details.

--- index.tsx
+++ index.tsx
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+
+const server = Bun.serve({
+    hostname: "localhost",
+    port: 4000,
+    fetch: fetchHandler,
+  });
+
+  console.log(
+    `Chat GPT Clone using Bun is running on ${server.hostname}:${server.port}`
+  );
+
+  function fetchHandler (request: Request): Response {
+    return new Response(Bun.file("imdex.html"))
+
+  }
